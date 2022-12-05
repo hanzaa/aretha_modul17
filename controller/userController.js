de98e705c3777d9099a250bd32f07b9ffa1c7f89
hanzaa <95400077+hanzaa@users.noreply.github.com>
2022-12-05 16:56:52 +0700

    Add files via upload

---
+++ controller/userController.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const db = require('../db.config/db.config')
+const jwt = require('jsonwebtoken');
+// const Auth = require('./auth')
+const cookieParser = require('cookie-parser');
+require("dotenv").config();
+const bcrypt = require('bcryptjs');
+SECRET = process.env.SECRET
+
+
+const register = async(req, res, next) => {
+    // * 7. silahkan ubah password yang telah diterima menjadi dalam bentuk hashing
+    const { username, email, password}= req.body
+    const hash = await bcrypt.hash(password,10)
+    // 8. Silahkan coding agar pengguna bisa menyimpan semua data yang diinputkan ke dalam database
+    try {
+        db.query(`INSERT INTO unhan_modul_17 VALUES (DEFAULT, $1, $2, $3)`, [username, email, hash])
+        res.send(`input succeded`)
+    }
+    catch(err) {
+            console.log(err.message)
+            return res.status(500).send(`input failed`)
+        }
+    }
+
+
+
+const login = async(req, res, next) => {
+    const {email, password}= req.body
+   
+    // 9. komparasi antara password yang diinput oleh pengguna dan password yang ada didatabase
+    try {
+        const user = await db.query(`SELECT * FROM unhan_modul_17 WHERE email=$1`, [email])
+        if(user){
+            const validPass = await bcrypt.compare(password,user.rows[0].password)
+            if (validPass) {
+                // 10. Generate token menggunakan jwt sign
+                let jwtSecretKey = process.env.SECRET;
+                let data = {
+                    id: user.rows[0].id,
+                    username: user.rows[0].username,
+                    email:user.rows[0].email,
+                    password:user.rows[0].password
+                }
+                const token = jwt.sign(data, jwtSecretKey);
+                
+                //11. kembalikan nilai id, email, dan username
+                 res.cookie("JWT", token, {httpOnly: true,sameSite: "strict"}).status(200).json({
+                    id: user.rows[0].id,
+                    username: user.rows[0].username,
+                    email:user.rows[0].email,
+                    token:token
+                    });
+            } else {
+                res.send('login failed')
+            }
+        }
+    }
+    catch (err) {
+        return res.send('login failed')
+    }
+    
+
+    
+}
+
+const logout = async(req, res, next) => {
+                
+    try {
+        return res.clearCookie('JWT').send('logout succeded')
+        // 14. code untuk menghilangkan token dari cookies dan mengembalikan pesan "sudah keluar dari aplikasi"  
+    } catch (err) {
+        console.log(err.message);
+        return res.status(500).send(err)
+    }
+            
+}
+
+const verify = async(req, res, next) => {
+    try {
+        // 13. membuat verify
+        data = req.verified
+        return register.status(200).send(data)
+    } catch (err) {
+        console.log(err.message);
+        return res.status(500).send(err)    
+    }
+}
+
+module.exports = {
+    register,
+    login,
+    logout,
+    verify
+}
